abd6559036185846e9d006cac6fff53e87c07634
hamup <gc.kai.g@gmail.com>
2018-06-26 17:13:34 +0900

    Add SetupScript & Usage

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ In this repository, we manage dotfiles and others.
 ## Usage
 ```
 $git clone https://github.com/hamup/dotfiles.git
+
+$setup.hh
 ```
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+#Initial Setting
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+VIMRC=$HOME/.vimrc
+GVIMRC=$HOME/.gvimrc
+VIMDIR=$HOME/.vim
+
+#Definition of Symbolic Link
+if [ -e  $VIMRC ]; then
+  echo "$VIMRC already exists"
+else
+  ln -s $SCRIPT_DIR/.vimrc $HOME
+fi
+
+if [ -e  $GVIMRC ]; then
+  echo "$GVIMRC already exists"
+else
+  ln -s $SCRIPT_DIR/.gvimrc $HOME
+fi
+
+if [ -e  $VIMDIR ]; then
+  echo "$VIMDIR already exists"
+else
+  ln -s $SCRIPT_DIR/.vim $HOME
+fi
+
+#Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
